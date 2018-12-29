4c2020eee53daa2e1db62b257191838444024648
Grzegorz Lagocki <grzesieklagocki@gmail.com>
2018-12-29 19:36:39 +0100

    Testy komponentu VolumeControl.

---
+++ src/components/VolumeControl/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class VolumeControl extends Component {
+  state = {
+    volume: "0.0",
+    step: 2
+  };
+
+  componentDidMount = () => {
+    this.setState({ step: this.props.steps[0] });
+  };
+
+  plus = () => {
+    this.incrementVolume(this.state.step);
+  };
+
+  minus = () => {
+    this.incrementVolume(-this.state.step);
+  };
+
+  incrementVolume = value => {
+    let vol = Number(this.state.volume) + Number(value);
+
+    if (vol < -95.5 || vol > 31.5) return;
+
+    this.setState({
+      volume: vol.toFixed(1)
+    });
+  };
+
+  changeStep = event => {
+    this.setState({ step: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <select onChange={this.changeStep}>
+          {this.props.steps.map(step => (
+            <option value={step}>{step}</option>
+          ))}
+        </select>
+        <h1>
+          volume: {this.state.volume > 0 ? "+" : ""}
+          {this.state.volume}dB
+        </h1>
+        <button onClick={this.plus}>+</button>
+        <button onClick={this.minus}>-</button>
+      </div>
+    );
+  }
+}
+
+export default VolumeControl;
